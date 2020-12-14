50d1540f5e93dab3b794701e22dc85bc1683cc22
QiangZiBro <qiangzibro@gmail.com>
2020-12-14 12:46:55 +0800

    fix test.sh bug

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 count=`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`
 
 echo 'start'
 for (( c=count; c>=1; c-- ))
 do
-      python3 benchmark_models.py -g $c&&
+      python3 benchmark_models.py -g $c &
 done
+wait
 echo 'end'
